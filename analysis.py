4152f40cd66ccb603c8cb9dd5205a9161ddb924f
YanjunChen329 <chenyanjun0329@outlook.com>
2019-11-14 15:02:28 -0600

    Big changes 1. Plot figures for sub and assist 2. Add some files for assist 3. Add some analysis functions

--- analysis.py
+++ analysis.py
@@ -3,6 +3,8 @@ import json
 from itertools import dropwhile, takewhile
 import networkx as nx
 import matplotlib.pyplot as plt
+import pandas as pd
+
 
 def getdata(filename):
 	with open(filename, "r") as csvfile:
@@ -10,6 +12,7 @@ def getdata(filename):
 		yield next(datareader)
 		for row in datareader:
 				yield row
+
 
 def extractSubstitutionData():
 	# for i in range(10):
@@ -33,6 +36,7 @@ def extractSubstitutionData():
 				if (row[3] == '8'):
 					datawriter.writerow(row)
 
+
 def extractSubstitutionDataYear(year):
 	with open("substitution_data_csv.csv", "r") as csvfile:
 		with open("substitution_data_" + year + "_csv.csv" , "w") as destfile:
@@ -43,6 +47,30 @@ def extractSubstitutionDataYear(year):
 				if row[1][1:3] == year:
 					datawriter.writerow(row)
 
+
+def extractAssistData():
+	with open("Play_by_Play_data_csv.csv", "r") as csvfile:
+		with open("assist_data_csv.csv", "w", newline='') as destfile:
+			datareader = csv.reader(csvfile)
+			datawriter = csv.writer(destfile)
+			datawriter.writerow(next(datareader))
+			for row in datareader:
+				print(row[0])
+				if (row[3] == '1'):
+					datawriter.write(row)
+
+
+def extractAssistDataYear(year):
+	with open("assist_data_csv.csv", "r") as csvfile:
+		with open("assist_data_" + str(year) + "_csv.csv", "w", newline='') as destfile:
+			datareader = csv.reader(csvfile)
+			datawriter = csv.writer(destfile)
+			datawriter.writerow(next(datareader))
+			for row in datareader:
+				if len(row) > 2:
+					if row[1][1:3] == str(year):
+						datawriter.writerow(row)
+
 def saveToJson(dict, fileName):
 	j = json.dumps(dict)
 	f = open(fileName,"w")
@@ -52,6 +80,7 @@ def saveToJson(dict, fileName):
 def readFromJson(fileName):
 	f = open(fileName,"r")
 	f.read()
+
 
 def parseDescription(row):
 	# if teamid == '1610612756':
@@ -69,6 +98,18 @@ def parseDescription(row):
 	except:
 		print(row)
 	# print (teamName)
+
+
+def parseAssistDescription(description):
+	try:
+		right_bracket = description.index("]")
+		message = description[right_bracket + 2:]
+		p1 = message.split(" ")[0]
+		p2 = message.split("Assist: ")[1].split(" ")[0]
+		return p2, p1
+	except:
+		print(description)
+
 
 def isTeam(row):
 	try:
@@ -97,10 +138,13 @@ def getSpurs():
 		# print (dict.keys())
 		createGraph(dict)
 
-def getTeamName(desc):
+def getTeamName(desc, assist=False):
 	left_bracket = desc.index("[")
 	right_bracket = desc.index("]")
-	teamName = desc[left_bracket + 1: right_bracket]
+	if not assist:
+		teamName = desc[left_bracket + 1: right_bracket]
+	else:
+		teamName = desc[left_bracket + 1: left_bracket + 4]
 	return teamName
 
 def getAllTeams18():
@@ -120,6 +164,26 @@ def getAllTeams18():
 			teams[team][p1][p2] += 1
 	saveToJson(teams, "sub_18.json")
 
+
+def getAssistDict(filename, outname):
+	with open(filename, 'r') as infile:
+		teams = {}
+		datareader = csv.reader(infile)
+		for row in datareader:
+			if "Assist: " not in row[4]:
+				continue
+			team = getTeamName(row[4], assist=True)
+			if team not in teams:
+				teams[team] = {}
+			p1, p2 = parseAssistDescription(row[4])
+			if p1 not in teams[team]:
+				teams[team][p1] = {}
+			if p2 not in teams[team][p1]:
+				teams[team][p1][p2] = 0
+			teams[team][p1][p2] += 1
+	saveToJson(teams, outname)
+
+
 selected_players = ['White', 'Anderson', 'Green', 'Mills', 'Belinelli', 'Aldridge', 'Gasol', 'Forbes', 'Bertans', 'Murray', 'Gay', 'Poeltl', 'DeRozan', 'Cunningham']
 players2014 =['Ginobili', 'Duncan', 'Parker', 'Bonner', 'Anderson', 'Splitter', 'Green', 'Leonard', 'Joseph', 'Diaw', 'Mills', 'Baynes', 'Belinelli']
 
@@ -137,20 +201,12 @@ def createGraph(dict):
 					G.add_edge(key, v, weight=dict[key][v])
 	# print(G.in_degree)
 	return G
-	# options = {
-	# 	# 'node_color': 'black',
-	# 	'node_size': 100,
-	# 	# 'width': 3,
-	# 	'labels': labels,
-	# }
-	# nx.draw_spring(G, **options)
-	# # nx.draw(G, **options)
-	#
-	# plt.show()
 
-# def createGraphs():
 
-# extractSubstitutionDataYear("18")
-# getSpurs()
-# getAllTeams18()
-# parseDescription("(6:07)[SAS] White Substitution replaced by Mills", '1610612756')
+if __name__ == '__main__':
+	# extractAssistDataYear("18")
+	getAssistDict("assist_data_18_csv.csv", "assist_18.json")
+	# extractSubstitutionDataYear("18")
+	# getSpurs()
+	# getAllTeams18()
+	# parseDescription("(6:07)[SAS] White Substitution replaced by Mills", '1610612756')
